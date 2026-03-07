f078caecda5b373501488ce87eb0857a1b14426c
arieen <arielleharyanto@gmail.com>
2026-03-06 21:50:57 -0800

    text

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Welcome to Arielle Haryanto's Feature Selection Algorithm!")
+print("Type in the name of the file you want to test:")
+print("Type the number of the algorithm you want to run:")

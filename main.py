d5468d5b8102d0d65d516f0f73317c4da44bec6b
arieen <arielleharyanto@gmail.com>
2026-03-08 14:25:15 -0700

    loading data

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-print("Welcome to Arielle Haryanto's Feature Selection Algorithm!")
-print("Type in the name of the file you want to test:")
-print("Type the number of the algorithm you want to run:")
+import sys
+
+def load_data(filename):
+    data = []
+    with open(filename, 'r') as f:
+        for line in f:
+            row = list(map(float, line.split()))
+            if row:
+                data.append(row)
+    return data
+
+def main():
+    print("Welcome to Arielle Haryanto's Feature Selection Algorithm!")
+    filename = input("Type in the name of the file to test: ").strip()
+    try:
+        data = load_data(filename)
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+        sys.exit(1)
+
+    num_instances = len(data)
+    num_features  = len(data[0]) - 1
+
+    print(f"\nThis dataset has {num_features} features (not including the class attribute), "
+          f"with {num_instances} instances.")
+
+if __name__ == '__main__':
+    main()
